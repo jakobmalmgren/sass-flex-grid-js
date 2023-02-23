d6597f861c1cecb076f022f3e4293946821ddda4
“jakob <jakobmalmgrem111@gmail.com>
2023-02-23 17:26:05 +0100

    started my project and done the nav, and the hero section, but need to improve some stuf

---
+++ index.js
@@ -0,0 +1,13 @@
+const hamburger_menu = document.querySelector(".burger-menu");
+const nav_menu = document.querySelector(".nav-menu");
+
+hamburger_menu.addEventListener("click", function () {
+  hamburger_menu.classList.toggle("active");
+  nav_menu.classList.toggle("active");
+});
+document.querySelectorAll(".nav-menu").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger_menu.classList.remove("active");
+    nav_menu.classList.remove("active");
+  })
+);
