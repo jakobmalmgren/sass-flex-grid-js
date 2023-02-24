1e616a1e7c0cbea8c71b52498a59c6d375c31d2e
“jakob <jakobmalmgrem111@gmail.com>
2023-02-24 18:10:53 +0100

    created a grid, but need to fix some parts of it before its done

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ hamburger_menu.addEventListener("click", function () {
   hamburger_menu.classList.toggle("active");
   nav_menu.classList.toggle("active");
 });
-document.querySelectorAll(".nav-menu").forEach((n) =>
+document.querySelectorAll(".nav-item").forEach((n) =>
   n.addEventListener("click", () => {
     hamburger_menu.classList.remove("active");
     nav_menu.classList.remove("active");
